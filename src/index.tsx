b96a3506d8b6fcd53506639f1f78a55b7efa5698
Mykola Maik <mykoluamaik@gmail.com>
2024-11-19 19:03:11 +0200

    setup global mui theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "@/styles/muiTheme";
 import RouterProvider from "@/containers/providers/RouterProvider/RouterProvider.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider />
+    <ThemeProvider theme={theme}>
+      <RouterProvider />
+    </ThemeProvider>
   </StrictMode>
 );
