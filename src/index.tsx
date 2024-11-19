7aab25697564093a5ef3ad09eb308ede70919ead
Mykola Maik <mykoluamaik@gmail.com>
2024-11-19 19:42:52 +0200

    setup redux with saga

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ThemeProvider } from "@mui/material/styles";
+import { Provider } from "react-redux";
+import store from "@/redux/store";
 import theme from "@/styles/muiTheme";
 import RouterProvider from "@/containers/providers/RouterProvider/RouterProvider.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
-      <RouterProvider />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <RouterProvider />
+      </ThemeProvider>
+    </Provider>
   </StrictMode>
 );
